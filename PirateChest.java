80ec1e26b0f137913f36cf68afa5f91177b37885
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-10-30 22:14:36 -0600

    working on calculating dimensions

--- PirateChest.java
+++ PirateChest.java
@@ -1,8 +1,45 @@
 //This program is intended to solve the pirate chest problem. Our retired pirate friend needs to hide his chest of treasure in the pond. He wants to know how big his chest can be and still be hidden beneath the water. That is the goal
+import java.util.*;
 
 public class PirateChest{
+int pondDepth = 2;
+int pondVolume;
+int chestUnits;
+int chestVolume;
+int height =1;
+int solution;
+// const int MAX_CHEST_LENGTH = 
 
 public static void main(String[] args){
-System.out.println("Hello!");
+
+System.out.println("Enter dimensions: ");
+Scanner input = new Scanner(System.in);
+System.out.println("Chest Length: ");
+int chestLength = input.nextInt();
+System.out.println("Chest Width: ");
+int chestWidth = input.nextInt();
+System.out.println("Pond Length: ");
+int pondLength = input.nextInt();
+System.out.println("Pond Width: ");
+int pondWidth = input.nextInt();
+
+CalculateDimensions(chestLength, chestWidth, pondLength, pondWidth);
+System.out.println(FindLargestPossibleChest(chestLength, chestWidth, pondLength, pondWidth));
+}
+
+public static void CalculateDimensions(int cLength, int cWidth, int pLength, int pWidth){
+    pondVolume = pLength * pWidth * pondDepth;
+    chestVolume = chestUnits = cLength * cWidth * height;
+    
+
+}
+
+public static int FindLargestPossibleChest(int chestLength, int chestWidth, int pondLength, int pondWidth){
+    if(chestLength > pondWidth || chestWidth > pondWidth || chestVolume > pondVolume){
+        return solution = 0;
+    }
+    else{
+        
+    }
 }
 }
