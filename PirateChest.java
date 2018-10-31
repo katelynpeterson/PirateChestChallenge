bb7730e5781c8b05be396810e05f79be013a2960
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-10-30 20:53:05 -0600

    create class and add to project

---
+++ PirateChest.java
@@ -0,0 +1,8 @@
+//This program is intended to solve the pirate chest problem. Our retired pirate friend needs to hide his chest of treasure in the pond. He wants to know how big his chest can be and still be hidden beneath the water. That is the goal
+
+public class PirateChest{
+
+public static void main(String[] args){
+System.out.println("Hello!");
+}
+}
