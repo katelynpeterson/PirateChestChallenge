86f8c2ffce496311d2971f943132f5e22d325612
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-10-31 18:55:57 -0600

    Add array for pond dimensions, refine variables

--- PirateChest.java
+++ PirateChest.java
@@ -2,44 +2,50 @@
 import java.util.*;
 
 public class PirateChest{
-static int pondDepth = 2;
-static int pondVolume;
-static int chestUnits;
-static int chestVolume;
-static int height =1;
-static int solution;
-// const int MAX_CHEST_LENGTH = 
+    static int pondVolume;
+    static int chestUnits;
+    static int chestVolume;
+    static int height =1;
+    static int solution;
+    static int chestLength;
+    static int chestWidth;
+    static int [][] pondDimensions;
 
-public static void main(String[] args){
-System.out.println("Enter dimensions: ");
-Scanner input = new Scanner(System.in);
-System.out.println("Chest Length: ");
-int chestLength = input.nextInt();
-System.out.println("Chest Width: ");
-int chestWidth = input.nextInt();
-System.out.println("Pond Length: ");
-int pondLength = input.nextInt();
-System.out.println("Pond Width: ");
-int pondWidth = input.nextInt();
+    public static void main(String[] args){
+        System.out.println("Enter dimensions: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Chest Length: ");
+        int maxChestLength = input.nextInt();
+        System.out.println("Chest Width: ");
+        int maxChestWidth = input.nextInt();
+        chestLength = Math.max(maxChestLength, maxChestWidth);
+        chestWidth = Math.min(maxChestLength, maxChestWidth);
+        System.out.println("Pond Length: ");
+        int pondLength = input.nextInt();
+        System.out.println("Pond Width: ");
+        int pondWidth = input.nextInt();
+        pondDimensions = new int[pondWidth][pondLength];
 
-CalculateDimensions(chestLength, chestWidth, pondLength, pondWidth);
-System.out.println(FindLargestPossibleChest(chestLength, chestWidth, pondLength, pondWidth));
-input.close();
-}
+        for(int i=0; i<chestWidth; i++)
+            for(j=0; j<chestLength; j++)
+                pondDimensions[i][j]= input.nextInt();
 
-public static void CalculateDimensions(int cLength, int cWidth, int pLength, int pWidth){
-    pondVolume = pLength * pWidth * pondDepth;
-    chestVolume = chestUnits = cLength * cWidth * height;
-    
-
-}
-
-public static int FindLargestPossibleChest(int chestLength, int chestWidth, int pondLength, int pondWidth){
-    if(chestLength > pondWidth || chestWidth > pondWidth || chestVolume > pondVolume){
-        return solution = 0;
+        CalculateDimensions(chestLength, chestWidth, pondLength, pondWidth);
+        System.out.println("The solution is: " + FindLargestPossibleChest(chestLength, chestWidth, pondLength, pondWidth));
+        input.close();
     }
-    else{
-        return solution;
+
+    public static void CalculateDimensions(int cLength, int cWidth, int pLength, int pWidth){
+        pondVolume = pLength * pWidth * pondDepth;
+        chestVolume = chestUnits = cLength * cWidth * height;
     }
-}
+
+    public static int FindLargestPossibleChest(int chestLength, int chestWidth, int pondLength, int pondWidth){
+        if(chestLength > pondWidth || chestWidth > pondWidth || chestVolume > pondVolume){
+            return solution = 0;
+        }
+        else{
+            return solution;
+        }
+    }
 }
